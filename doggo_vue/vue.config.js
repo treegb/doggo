2d29144ec29c0786c1e4a70b0e7b2f6ad11f068a
treegb <bambooindark@gmail.com>
2019-04-29 23:05:54 +0800

    cd inside "dist" dir, and try to push to gh-pages

--- doggo_vue/vue.config.js
+++ doggo_vue/vue.config.js
@@ -1,5 +1,10 @@
 module.expots = {
   configureWebpack: {
       devtool: 'source-map'
+      // .Maybe I should comment this out for production environment.
   },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/doggo/'
+    : '/'
+  // .For deploy to github pages.
 }
